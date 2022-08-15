f5301fd4a381c8c0dede45d4fd2400f02e2ce1d0
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-15 17:28:55 +0900

    feat: create job-posting entity and relation with company

--- src/company/entities/company.entity.ts
+++ src/company/entities/company.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { JobPosting } from 'src/job-posting/entities/job-posting.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Company {
@@ -7,4 +8,7 @@ export class Company {
 
   @Column()
   name: string;
+
+  @OneToMany(() => JobPosting, (jobPosting) => jobPosting.company)
+  jobPostings: JobPosting[];
 }
--- src/job-posting/entities/job-posting.entity.ts
+++ src/job-posting/entities/job-posting.entity.ts
@@ -1,1 +1,29 @@
-export class JobPosting {}
+import { Company } from 'src/company/entities/company.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class JobPosting {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  country: string;
+
+  @Column()
+  region: string;
+
+  @Column()
+  position: string;
+
+  @Column()
+  compensation: number;
+
+  @Column()
+  technique: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => Company, (company) => company.jobPostings)
+  company: Company;
+}

4708af02dc899f79d05c6047fb61cbd716cc8362
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-15 16:36:26 +0900

    feat: generate company resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [],
+  imports: [CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })

193eb9439b67532ad74265be2a9f25206e5798a9
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-08-15 16:39:35 +0900

    feat: create Company entity
    - create PK id
    - create column name

--- src/company/entities/company.entity.ts
+++ src/company/entities/company.entity.ts
@@ -1,1 +1,10 @@
-export class Company {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Company {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
